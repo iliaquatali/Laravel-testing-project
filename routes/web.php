cb506528f980b091814379a549e0f2b2842ad729
Liaquat Ali <liaquatali.centosquare@gmail.com>
2022-12-23 10:38:24 +0500

    except edit

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 Route::view('', 'admin.dashboard.index')->name('admin.dashboard');
 
-
 Route::controller( UserController::class)
     ->prefix('admin/user')
     ->name('user.')
